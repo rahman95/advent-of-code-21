fb0c6fc0a7eeebfec652cbb3648b83e5b638e308
Rahman Younus <rahman.younus@dazn.com>
2021-12-03 17:26:47 +0000

    completed day1 - part 2

--- day1/index.js
+++ day1/index.js
@@ -1,41 +1,91 @@
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
-// Example
-// const inputArray = [
-//   199, // (N/A - no previous measurement)
-//   200, // (increased)
-//   208, // (increased)
-//   210, // (increased)
-//   200, // (decreased)
-//   207, // (increased)
-//   240, // (increased)
-//   269, // (increased)
-//   260, // (decreased)
-//   263, // (increased)
-// ];
+const readFile = () => {
+  const data = fs.readFileSync("input.txt", "utf8");
+  return data
+    .toString()
+    .split("\n")
+    .map((numberAsString) => parseInt(numberAsString));
+};
 
-const data = fs.readFileSync("input.txt", "utf8");
-const inputArray = data
-  .toString()
-  .split("\n")
-  .map((numberAsString) => parseInt(numberAsString));
+const part1 = () => {
+  const inputArray = readFile();
 
-let increasedCount = 0;
-let decreasedCount = 0;
-inputArray.forEach((depthMeasurement, index) => {
-  // ignore first
-  if (index === 0) {
-    return;
-  }
+  let increasedCount = 0;
+  let decreasedCount = 0;
+  inputArray.forEach((depthMeasurement, index) => {
+    // ignore first
+    if (index === 0) {
+      return;
+    }
 
-  let lastNumber = inputArray[index - 1];
-  if (depthMeasurement > lastNumber) {
-    increasedCount++;
-  }
+    let lastNumber = inputArray[index - 1];
+    if (depthMeasurement > lastNumber) {
+      increasedCount++;
+    }
 
-  if (depthMeasurement < lastNumber) {
-    decreasedCount++;
-  }
-});
+    if (depthMeasurement < lastNumber) {
+      decreasedCount++;
+    }
+  });
 
-console.log({ inputCount: inputArray.length, increasedCount, decreasedCount });
+  console.log({
+    inputCount: inputArray.length,
+    increasedCount,
+    decreasedCount,
+  });
+};
+
+const getValue = (array, index) => {
+  if (array[index] === undefined) return 0;
+
+  return array[index];
+};
+
+const part2 = () => {
+  const inputArray = readFile();
+
+  const adjustedInput = [];
+  inputArray.forEach((depthMeasurement, index) => {
+    adjustedInput.push({
+      identifier: uuidv4(),
+      values: [
+        getValue(inputArray, index),
+        getValue(inputArray, index + 1),
+        getValue(inputArray, index + 2),
+      ],
+      sum:
+        getValue(inputArray, index) +
+        getValue(inputArray, index + 1) +
+        getValue(inputArray, index + 2),
+    });
+  });
+
+  let increasedCount = 0;
+  let decreasedCount = 0;
+  adjustedInput.forEach((currentIteration, index) => {
+    // ignore first
+    if (index === 0) {
+      return;
+    }
+
+    let lastIteration = adjustedInput[index - 1];
+    if (currentIteration.sum > lastIteration.sum) {
+      increasedCount++;
+    }
+
+    if (currentIteration.sum < lastIteration.sum) {
+      decreasedCount++;
+    }
+  });
+
+  console.log({
+    inputCount: inputArray.length,
+    increasedCount,
+    decreasedCount,
+  });
+};
+
+// part1();
+// part2();
